845238ccdff8c86831fb85a8727cba3624f1054a
ikuekashima <ziaord-@docomo.ne.jp>
2025-01-11 13:22:12 +0900

    kadai-lesson3

---
+++ src/sample/Review01.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class Review01 {
+
+    public static void main(String[] args) {
+
+//1500円の商品の税込価格は1650円（消費税は○○円）です。
+     int price = 1500;
+     double tax = price*0.1;
+     double pay = price + tax;
+     System.out.println(price+"円の商品の税込価格は"+pay+"円（消費税は"+tax+"円)です");
+
+
+
+    }
+}
